daa7e64fbab69b6679575b3ad3d1854212d3d657
Piyush Nalawade <nalawadepiyush@gmail.com>
2018-08-05 04:14:44 +0530

    Reformatting of file

--- flask_app.py
+++ flask_app.py
@@ -1,79 +1,92 @@
-from flask import Flask, render_template, request, redirect, url_for, session, g, Response
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def homepage():
     return render_template("homepage.html")
 
 
-@app.route('/start', methods=['GET','POST'])
+@app.route('/start', methods=['GET'])
 def start():
-    browser_name = request.form['browser']
-    url = request.form['url']
-    print browser_name , url
-    if browser_name=="chrome" and url:
-        import webbrowser
-        urll = url
-        # Linux
-        chrome_path = '/usr/bin/google-chrome %s'
-        webbrowser.get(chrome_path).open(urll)
-    elif browser_name=="mozilla" and url:
-        import webbrowser
-        urll = url
-        # Linux
-        mozilla_path = '/usr/bin/firefox %s'
-        webbrowser.get(mozilla_path).open(urll)
-    elif browser_name == "chrome":
-        import webbrowser
-        urll = url
-        # Linux
-        chrome_path = '/usr/bin/google-chrome %s'
-        webbrowser.get(chrome_path).open("https://www.google.com")
-    elif browser_name == "mozilla":
-        import webbrowser
-        urll = url
-        # Linux
-        chrome_path = '/usr/bin/firefox %s'
-        webbrowser.get(chrome_path).open("https://www.google.com")
-    elif url:
-        return "no browser name detected"
-    else:
-        return "nothing to fire up"
+    if 'browser' in request.args and 'url' in request.args:
+        print "fegerge"
+        browser_name = str(request.args['browser'])
+        url = str(request.args['url'])
+        print browser_name
+        print url
+        if browser_name == "chrome":
+            import webbrowser
+            chrome_path = '/usr/bin/google-chrome %s'
+            webbrowser.get(chrome_path).open(url)
+            return "Chrome fired up with specified url"
+        elif browser_name == "mozilla":
+            import webbrowser
+            mozilla_path = '/usr/bin/firefox %s'
+            webbrowser.get(mozilla_path).open(url)
+            return "Mozilla fired up with specified url"
+        else:
+            return "This browser isnt available"
+    elif 'browser' in request.args:
+        browser_name = str(request.args['browser'])
+        if browser_name == "chrome":
+            import webbrowser
+            chrome_path = '/usr/bin/google-chrome %s'
+            webbrowser.get(chrome_path).open("https://www.google.com")
+            return "Chrome fired up"
+        elif browser_name == "mozilla":
+            import webbrowser
+            mozilla_path = '/usr/bin/firefox %s'
+            webbrowser.get(mozilla_path).open("https://www.google.com")
+            return "Mozilla fired up "
+        else:
+            return "This browser isnt available"
+    elif 'url' in request.args:
+        return "Error only url provided"
 
 
-@app.route('/stop', methods=['GET','POST'])
+@app.route('/stop', methods=['GET'])
 def stop():
-    browser_name = request.form['browser']
-    print browser_name
-    if browser_name == "chrome":
-        import os
-        browserExe = "chrome"
-        os.system("pkill " + browserExe)
-    elif browser_name == "mozilla":
-        import os
-        os.system("pkill " + "firefox")
-        return "closed"
+    if 'browser' in request.args:
+        browser_name = str(request.args['browser'])
+        print browser_name
+        if browser_name == "chrome":
+            import os
+            browserExe = "chrome"
+            os.system("pkill " + browserExe)
+            return "Chrome closed"
+        elif browser_name == "mozilla":
+            import os
+            os.system("pkill " + "firefox")
+            return "Mozilla closed"
+        else:
+            return "This browser isnt available"
     else:
-        return "nothing to close"
+        return "Browser name required"
 
 
-@app.route('/cleanup', methods=['GET', 'POST'])
+@app.route('/cleanup', methods=['GET'])
 def cleanup():
-    browser_name = request.form['browser']
-    if browser_name=="chrome":
-        import os
-        os.system("rm -rf ~/.cache/google-chrome/")
-        return "Chrome Cache cleared "
-    elif browser_name=="mozilla":
-        import os
-        os.system("rm -rf ~/.cache/mozilla/")
-        return "Mozilla Cache cleared"
-
-# @app.route('/getmyurl', methods=['GET', 'POST'])
-# def cleanup():
-#     return "456"
+    if 'browser' in request.args:
+        browser_name = str(request.args['browser'])
+        print browser_name
+        if browser_name == "chrome":
+            # Successfully clears chrome content
+            import os
+            os.system("rm -rf ~/.cache/google-chrome/Default/Cache")
+            os.system("rm -rf ~/.cache/google-chrome/Default/")
+            os.system("rm -rf ~/.config/google-chrome/")
+            os.system("rm -rf ~/.config/google-chrome/Default")
+            return "Cleared Chrome Data"
+        elif browser_name == "mozilla":
+            # Successfully clears mozilla content
+            import os
+            os.system("rm -rf ~/.cache/mozilla/")
+            os.system("rm -rf ~/.mozilla/")
+            return "Cleared Mozilla Data"
+    else:
+        return "No query passed for filtration"
 
 
 if __name__ == '__main__':
